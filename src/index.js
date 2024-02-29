5d4a31c762af421695cc441f21b344dd8ecf6b9d
Josh <jzumbrunn@yahoo.com>
2024-02-29 14:37:59 -0500

    mic access

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+/*
+Steps
+1. Get Access to Microphone
+2. Capture Audio Data
+3. Analyze Frequency
+4. Match Frequency to a Note
+5. Create a User Interface
+*/
+
+// 1. Get Access to Microphone
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then((stream) => {
+    console.log(stream);
+  })
+  .catch((err) => {
+    throw new Error("Unable to gain access");
+  });
+// 2. Capture Audio Data
+// 3. Analyze Frequency
+// 4. Match Frequency to a Note
+
+// 5. Create a User Interface
 const render = () => {
   const App = document.createElement("div");
 
@@ -6,18 +29,17 @@ const render = () => {
   title.innerText = "Guitar Tuner App";
 
   // Tuner
-  const Tuner = document.createElement('div')
+  const Tuner = document.createElement("div");
 
-  const tunerTitle = document.createElement('h3')
-  tunerTitle.innerText = 'Tune Guitar Here'
+  const tunerTitle = document.createElement("h3");
+  tunerTitle.innerText = "Tune Guitar Here";
 
   // Tuner UI
-  const ui = document.createElement('div')
-  ui.innerText = 'Tuner UI'
-
+  const ui = document.createElement("div");
+  ui.innerText = "Tuner UI";
 
   const root = document.getElementById("root");
-  Tuner.append(tunerTitle)
+  Tuner.append(tunerTitle);
   App.append(title, Tuner, ui);
   root.append(App);
 };
