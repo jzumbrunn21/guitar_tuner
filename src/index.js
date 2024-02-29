df42591cefd8383f3769a1de79a569a0d12c96d2
Josh <jzumbrunn@yahoo.com>
2024-02-29 13:56:32 -0500

    skeleton setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const render = () => {
   const App = document.createElement("div");
-  App.innerText = "Howdy World from Dev!";
+
+  // Title
+  const title = document.createElement("h1");
+  title.innerText = "Guitar Tuner App";
+
+  // Tuner
+  const Tuner = document.createElement('div')
+
+  const tunerTitle = document.createElement('h3')
+  tunerTitle.innerText = 'Tune Guitar Here'
+
+  // Tuner UI
+  const ui = document.createElement('div')
+  ui.innerText = 'Tuner UI'
+
 
   const root = document.getElementById("root");
+  Tuner.append(tunerTitle)
+  App.append(title, Tuner, ui);
   root.append(App);
 };
 
